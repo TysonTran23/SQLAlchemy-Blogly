85009df88f803ba22017bd746fbc9d91cc469123
Tyson Tran <tysontran@Tysons-MBP.ht.home>
2023-07-03 16:31:03 -0700

    wrote one test code

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from app import app
-from models import User, db, Post
+from models import Post, User, db
 
 # Use test database and don't clutter tests with SQL
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly_test"
@@ -14,27 +14,32 @@ app.config["TESTING"] = True
 # This is a bit of hack, but don't use Flask DebugToolbar
 app.config["DEBUG_TB_HOSTS"] = ["dont-show-debug-toolbar"]
 
-db.drop_all()
-db.create_all()
-
 
 class UserTestCase(TestCase):
     def setUp(self):
-        User.query.delete()
+        db.drop_all()
+        db.create_all()
+
+        self.client = app.test_client()
+
+        # User.query.delete()
+
         default_url = "https://i.natgeofe.com/k/d2701667-c426-4a1b-8d75-d01bdc387fdc/vietnam-ha-long-bay.jpg?w=1084.125&h=609"
 
         user = User(first_name="tyson", last_name="tran", image_url=default_url)
         db.session.add(user)
         db.session.commit()
 
-        post = Post(title='Canada wins olympics', content='We won gold!', user_id=1)
+        post = Post(title="Canada wins olympics", content="We won gold!", user_id=1)
+
+        db.session.add(post)
+        db.session.commit()
 
         self.user = user
         self.user.id = user.id
-        
+
         self.post = post
         self.post.id = post.id
-        
 
     def tearDown(self):
         """Clean up any fouled transaction."""
@@ -58,23 +63,20 @@ class UserTestCase(TestCase):
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
-            self.assertIn('<a href="/users/1">tyson tran</a>', html)
-    
+            self.assertIn('<a href="/users/2">tyson tran</a>', html)
+
     def test_show_user_detail(self):
         with app.test_client() as client:
-
-            resp = client.get(f'/users/{self.user.id}')
+            resp = client.get(f"/users/{self.user.id}")
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
-            self.assertIn('<h1>tyson tran</h1>', html)
+            self.assertIn("<h1>tyson tran</h1>", html)
 
     def test_show_post_detail(self):
         with app.test_client() as client:
-
-            resp = client.get(f'/posts/{self.post.id}')
+            resp = client.get(f"/posts/{self.post.id}")
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
-            self.assertIn('<h1>Canada wins olympics</h1>', html)
-
+            self.assertIn("<h1>Canada wins olympics</h1>", html)
